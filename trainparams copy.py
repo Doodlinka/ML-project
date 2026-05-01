56a464e8c2e6d6f41d2442ed246a80940d7dd1cd
Doodlinka <oleshko.darya@gmail.com>
2026-05-01 20:34:27 +0300

    update params

--- trainparams copy.py
+++ trainparams copy.py
@@ -1,21 +1,23 @@
 import os
+AMWAUG = 15
 PROJDIR = os.path.abspath(os.path.dirname(__file__))
 MODEL = os.path.join(PROJDIR, 'yolov8n-seg.pt')
-CONFNAME = os.path.join(PROJDIR, "conf_nano_v1_15k.yaml")
-DATASET = os.path.join(PROJDIR, "train_15000_split.txt")
+CONFNAME = os.path.join(PROJDIR, f"conf_nano_v1_{AMWAUG}k.yaml")
+DATASET = os.path.join(PROJDIR, f"train_{AMWAUG}000_split.txt")
+
 PARAMS = {
     "data": CONFNAME,
     # disable per-pixel cause it's done offline
     "hsv_h": 0.0,      
     "hsv_s": 0.0,      
-    "hsv_v": 0.0,      
-    "blur": 0.0,       
-    "median": 0,       
+    "hsv_v": 0.0,            
 
-    "device": "cpu",
+    "device": 0,
     
-    "name": 'nano_v1_15k',
-    "epochs": 5,
+    "name": f'nano_v2_{AMWAUG}k',
+    "cls": 2,
+    "epochs": 10,
     "imgsz": 640,
-    "batch": 16,
+    "batch": 12,
+    "workers": 5,
 }
